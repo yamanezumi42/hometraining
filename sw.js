e095af6d94dcb8bf930f103af51a147d5744c74f
Sato <yamanezumi42@gmail.com>
2026-06-17 22:38:57 +0900

    自宅トレ A/B タイマー＋記録アプリ v1

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "ht-v1-0";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const cp = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp));
+      return resp;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
